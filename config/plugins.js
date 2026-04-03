041cb6ed22525222a10c062bad9c261c979b6e24
Arthur Moreau <kibtpwd@gmail.com>
2026-04-03 12:11:09 +0200

    Refactor email provider configuration in plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -3,13 +3,13 @@ module.exports = () => ({
     config: {
       provider: 'sendgrid', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
       providerOptions: {
-        apiKey: env('SENDGRID_API_KEY'),
-      },
+        apiKey: env('SENDGRID_API_KEY')
+      }
       settings: {
         defaultFrom: 'arthur@bwop.io',
         defaultReplyTo: 'arthur@bwop.io',
-        testAddress: 'arthur@bwop.io',
-      },
-    },
-  },
+        testAddress: 'arthur@bwop.io'
+      }
+    }
+  }
 });
