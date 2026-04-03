bf44167f94ed957dd8baddc1fe6cd633103613ea
Arthur Moreau <kibtpwd@gmail.com>
2026-04-03 12:11:18 +0200

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,6 @@
 module.exports = () => ({
   email: {
     config: {
-      provider: 'sendgrid', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
-      providerOptions: {
-        apiKey: env('SENDGRID_API_KEY')
-      }
       settings: {
         defaultFrom: 'arthur@bwop.io',
         defaultReplyTo: 'arthur@bwop.io',
